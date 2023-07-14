de6bc101b58c54329f43c2cb56ae22359c3f2c87
Cody McEachern <113365765+cmacspeedo@users.noreply.github.com>
2023-07-14 00:13:58 +0000

    initial commit

--- exercise.py
+++
@@ -1,3 +0,0 @@
-birth_year = input("Enter your birthyear: ")
-age = 2023 - int(birth_year)
-print(age)
--- madlib.py
+++
@@ -1,5 +0,0 @@
-noun = input("What is a noun? (Fish, Dog, Cat)")
-verb = input ("Add a verb (jump, run, fall)")
-plural_noun = input ("Add a plural noun (men, Mice, knives)")
-
-print (f"The {noun} jumps over the fence. Then {verb} to the big red house, / finaly stopping to catch their breathe, they invited {plural_noun} to the garage.")
